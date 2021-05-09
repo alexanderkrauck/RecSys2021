3af38d407c5d05ab76b5f44cc9803c9447bfd0b6
Alexander Krauck <alexander.krauck@gmail.com>
2021-05-09 21:15:09 +0200

    docker environment

---
+++ Docker/utils/features.py
@@ -0,0 +1,103 @@
+import numpy as np
+from collections import Counter
+from typing import Tuple
+from .constants import __media_type_mapping, __type_mapping, __language_mapping
+
+import dask.dataframe as dd
+from typing import List
+
+#from transformers import BertTokenizer
+
+from datetime import datetime
+
+#tokenizer = BertTokenizer.from_pretrained("bert-base-multilingual-cased")
+
+def save_log(item):
+    return np.log(abs(item) + 1)
+
+div = 2**32
+single_column_features = {
+    # TODO: apply log to numerical values (or not?)
+    # name of the feature : ([required columns], function, output type,
+    #   apply level (1: series lambda/apply, 2: df row lambda, 3: df map_partitions))
+    # also highly recommended to specify at least one of the features from all_labels in the requirements even if not
+    #   needed for computation for all target derived features.
+    "bert_token_len": ('bert_base_multilingual_cased_tokens', lambda bertenc: save_log(len(bertenc.split('\t'))), np.uint32, 1),
+    "has_reply": ('reply', lambda v: v > 0., bool, 3),
+    "has_retweet": ('retweet', lambda v: v > 0., bool, 3),
+    "has_retweet_comment": ('retweet_comment', lambda v: v > 0., bool, 3),
+    "has_like": ('like', lambda v: v > 0., bool, 3),
+    "n_photos": ('medias', lambda v: save_log(Counter(v.split('\t'))['Photo']) if v else 0, np.float32, 1),
+    "n_videos": ('medias', lambda v: save_log(Counter(v.split('\t'))['Video']) if v else 0, np.float32, 1),
+    "n_gifs": ('medias', lambda v: save_log(Counter(v.split('\t'))['GIF']) if v else 0, np.float32, 1),
+    "reply_age": (['reply', 'timestamp', 'has_reply'], lambda df: (df['reply']-df['timestamp'])*df['has_reply'], np.uint32, 3),
+    "like_age": (['like', 'timestamp', 'has_like'], lambda df: (df['like']-df['timestamp'])*df['has_like'], np.uint32, 3),
+    "retweet_age": (['retweet', 'timestamp', 'has_retweet'], lambda df: (df['retweet']-df['timestamp'])*df['has_retweet'], np.uint32, 3),
+    "retweet_comment_age": (['retweet_comment', 'timestamp', 'has_retweet_comment'], lambda df: (df['retweet_comment']-df['timestamp'])*df['has_retweet_comment'], np.uint32, 3),
+    #"bert_char_len": ('bert_base_multilingual_cased_tokens', lambda bertenc: len(tokenizer.convert_tokens_to_string(tokenizer.convert_ids_to_tokens(bertenc.split("\t")))), np.uint16, 1), #SLOW
+    "type_encoding": ('type', lambda x: __type_mapping[x], np.uint8, 1),
+    "language_encoding": ('language', lambda x: __language_mapping[x], np.uint16, 1),
+    "a_followers" :('a_follower_count', save_log, np.float32, 1),
+    "a_following" :('a_following_count', save_log, np.float32, 1),
+    "b_followers" :('b_follower_count', save_log, np.float32, 1),
+    "b_following" :('b_following_count', save_log, np.float32, 1),
+    "day_of_week" :('timestamp', lambda x: datetime.fromtimestamp(x).weekday(), np.uint8, 1),
+    "hour_of_day":('timestamp', lambda x: datetime.fromtimestamp(x).hour, np.uint8, 1),
+    "b_creation_delta": (['timestamp', 'b_account_creation'], lambda x: save_log(x["timestamp"] - x["b_account_creation"]), np.float32, 3),
+    "a_creation_delta": (['timestamp', 'a_account_creation'], lambda x: save_log(x["timestamp"] - x["a_account_creation"]), np.float32, 3),
+    "tweet_hash": ("tweet_id", lambda x: int(x, 16)%div , np.uint32, 1),#this introduces a slight error but should be fine with the big picture...
+    "b_hash": ("b_user_id", lambda x: int(x, 16)%div , np.uint32, 1),
+    "a_hash": ("a_user_id", lambda x: int(x, 16)%div , np.uint32, 1)
+}
+
+
+def TE_get_name(feature_name: str, target_name: str) -> str:
+    return 'TE_'+feature_name+'_'+target_name
+
+
+def TE_dataframe_dask(df: dd.DataFrame,
+                      feature_name: str,
+                      target_name: str,
+                      w_smoothing: int = 20,
+                      dt=np.float64,
+                      counts_and_means: dd.DataFrame = None) -> Tuple[dd.DataFrame]:
+    if counts_and_means is None:
+        counts_and_means = df[[target_name, feature_name]].groupby(feature_name)[target_name].agg(['count', 'mean'])
+        counts_and_means["total_mean"] = df[target_name].mean() # redundant but necessary
+    TE_map = counts_and_means.apply(lambda cm: (cm["count"]*cm["mean"]+w_smoothing*cm["total_mean"])/(cm["count"]+w_smoothing),
+                                    axis=1,
+                                    meta=(TE_get_name(feature_name, target_name), dt)
+                                    )
+    # the only way to vectorize this, joining on non-index - must be somewhat costly
+    df = df.join(TE_map, on=feature_name, how='left')
+    df = df.fillna(-1)#for test set...
+
+    return df, counts_and_means    # all lazy all dask, should be fine, evaluated in the end when all the things are merged
+
+
+def conditional_probabilities(df: dd.DataFrame,
+                              features: List[str],
+                              labels: List[str]) -> dd.Series:
+    '''
+
+    Parameters
+    ----------
+    df
+        Dask Dataframe with the data
+    features
+        the categorical feature labels that are to be included in the computation
+    labels
+        the categorical labels that are to be included in the computation
+
+    Returns
+    -------
+        A lazy series, indexed on values of features and labels and contains the conditional probabilities of
+            the features.
+    '''
+    # FIXME: does not support supplying new features only due the usage of tweet_id, find or make another column that
+    #   works well as an omnipresent pivot
+    marg = df[labels+["tweet_id"]].groupby(labels)["tweet_id"].count().rename('marginal').to_frame() # because tweet_id is always there
+    joint = df[labels+features+['tweet_id']].groupby(labels+features)['tweet_id'].count().rename('joint').to_frame()
+    lp = joint.join(marg, on=labels, how='left')
+    pp = lp.joint / lp.marginal
+    return pp
---
+++ Docker/utils/model.py
@@ -0,0 +1,178 @@
+"""
+Utility classes for models for Recsys2021 challenge
+
+"""
+
+__author__ = "Alexander Krauck"
+__email__ = "alexander.krauck@gmail.com"
+__date__ = "08-03-2021"
+
+
+import os
+from os.path import join
+from pathlib import Path
+import torch
+from torch import nn
+import xgboost as xgb
+import pandas as pd
+import numpy as np
+
+#Convention Imports
+from abc import ABC, abstractmethod
+from typing import Union, List, Tuple, Callable, Dict, Iterable
+
+class RecSys2021BaseModel(ABC):
+    """Base class to inherit from for the Recsys2021 challenge"""
+
+    def evaluate_test_set(self, testLoader: Iterable, output_file: str = "output.csv"):
+        """Evaluates the test data inside the testLoader and otputs it appropriatly
+
+        Parameters
+        ----------
+        testLoader: Iterable
+            The dataloader for the test data. The dataloader is expected to always give the tweet_ids and 
+            user_ids (engaging user id = user b) with the batch.
+        output_file: str
+            The filepath where the results will be written in the appropriate format.
+        
+        """
+
+
+        with open(output_file, 'a') as output:
+            for (tweet_ids, user_ids), batch in testLoader:
+                reply_preds, retweet_preds, retweet_comment_preds, like_preds = self.infer(batch)
+
+                for tweet_id, user_id, reply_pred, retweet_pred, retweet_comment_pred, like_pred in \
+                    zip(tweet_ids, user_ids, reply_preds, retweet_preds, retweet_comment_preds, like_preds):
+
+                    output.write(f'{tweet_id},{user_id},{reply_pred},{retweet_pred},{retweet_comment_pred},{like_pred}\n')
+        
+    
+    @abstractmethod
+    def infer(self, x: Union[List, torch.Tensor, Tuple, Dict]) -> Tuple[Iterable, Iterable, Iterable, Iterable]:
+        """In the infer call the model should process 1 batch
+        
+        Parameters
+        ----------
+        x: Union(List, torch.Tensor, Tuple, Dict)
+            The input data
+        
+        Returns
+        ----------
+        batch_prediction: Tuple[Iterable, Iterable, Iterable, Iterable]
+            The biary predictions of (reply_preds, retweet_preds, retweet_comment_preds, like_preds).
+        """
+        
+        raise NotImplementedError("This is abstract!")
+
+
+class SimpleFFN(torch.nn.Module):
+    def __init__(self, n_input_features: int):
+        super(SimpleFFN, self).__init__()
+        self.net = nn.Sequential(
+            nn.Linear(n_input_features, 128),
+            nn.ReLU(),
+            nn.Dropout(.2),
+            nn.Linear(128, 128),
+            nn.ReLU(),
+            nn.Dropout(),
+            nn.Linear(128, 1),#Just one output!
+            nn.Sigmoid()#Because we need binary loss
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+
+class RecSysNeural1(torch.nn.Module, RecSys2021BaseModel):
+    def __init__(self, n_input_features: int):
+        super(RecSysNeural1, self).__init__()
+
+        self.like_net = SimpleFFN(n_input_features)
+        self.reply_net = SimpleFFN(n_input_features)
+        self.retweet_comment_net = SimpleFFN(n_input_features)
+        self.retweet_net = SimpleFFN(n_input_features)
+
+    def forward(self, x):
+        return self.reply_net(x), self.retweet_net(x) ,self.retweet_comment_net(x), self.like_net(x)
+
+    def infer(self, x):
+        raise NotImplementedError("Implement this!")
+
+
+class RecSysXGB1(RecSys2021BaseModel):
+
+    def __init__(self, model_dir: str = None):
+        self.clfs_ = {}
+        self.targets__ = ['has_reply', 'has_retweet', 'has_retweet_comment', 'has_like']
+
+
+        if model_dir is not None:
+            for filename in os.listdir(model_dir):
+                booster = xgb.Booster()
+                booster.load_model(join(model_dir,filename))
+                self.clfs_[filename] = booster
+
+                
+
+    def train_in_memory(self,
+            train_set: pd.DataFrame,
+            feature_columns: List,
+            xgb_parameters: dict,
+            save_dir: str = None
+        ):
+        """Train in-memory with a pandas train set. 
+        
+        The train_set is expected to have 4 target columns with names ['has_reply', 'has_retweet', 'has_retweet_comment', 'has_like']
+
+        Parameters
+        ----------
+        train_set: pd.DataFrame
+            Pandas dataframe with features and targets.
+        feature_columns: List
+            List of column names of the train_set that should be used as training features (X) for the models
+        xgb_parameters: dict
+            The configuration for the XGB model as specified in https://xgboost.readthedocs.io/en/latest/parameter.html
+        save_dir: str
+            The directory where models will be stored if given
+        """
+        
+        
+        for target in self.targets__:
+            dtrain = xgb.DMatrix(train_set[feature_columns], label=train_set[target])
+            clf = xgb.train(xgb_parameters, dtrain, 10)
+
+            self.clfs_[target] = clf
+            if save_dir is not None:
+                Path(save_dir).mkdir(parents=True, exist_ok=True)
+                clf.save_model(join(save_dir, target))
+        
+            
+    def infer(self, x, feature_columns = None):
+        if feature_columns is not None:
+            x = x[feature_columns]
+
+        x = xgb.DMatrix(x)
+
+        results = []
+        for key in self.targets__:
+            results.append(self.clfs_[key].predict(x))
+        
+        return tuple(results)
+
+
+
+
+"""Functions copied from https://recsys-twitter.com/code/snippets"""
+from sklearn.metrics import average_precision_score, log_loss
+def calculate_ctr(gt):
+  positive = len([x for x in gt if x == 1])
+  ctr = positive/float(len(gt))
+  return ctr
+
+def compute_rce(pred, gt):
+    cross_entropy = log_loss(gt, pred)
+    data_ctr = calculate_ctr(gt)
+    strawman_cross_entropy = log_loss(gt, [data_ctr for _ in range(len(gt))])
+    return (1.0 - cross_entropy/strawman_cross_entropy) * 100.0
+  
