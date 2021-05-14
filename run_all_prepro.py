2097eb03487d73393a65d478e6eeb20b5fb1fd39
alexanderkrauck <alexander.krauck@gmail.com>
2021-05-14 10:46:41 +0200

    Mach2 ready

--- run_all_prepro.py
+++ run_all_prepro.py
@@ -1,14 +1,41 @@
-from utils.preprocess import preprocess, get_dask_compute_environment, load_all_preprocessed_data
-from dask.distributed import progress
+from utils.preprocess import preprocess
+import utils.config as config
+from dask.distributed import progress, wait
+import utils.compute_and_front as compute
+
+
+
 
 
 if __name__=='__main__':
-    _, delayed_save = preprocess()
 
-    with get_dask_compute_environment() as client:
-        f = client.persist(delayed_save)
+
+    cfg = config.load_config("mach2_train_config.yaml")
+
+    ddf, delayed_dump, other_delayed = preprocess.preprocess(cfg)
+
+    compconf = cfg['compute']
+    compconf['verbose'] = cfg['verbose']
+
+    with compute.get_dask_compute_environment(compconf) as client:
+        f = client.persist([delayed_dump, *other_delayed])
+        progress(f)
+        wait(f)
+
+    
+
+    print("Training Data Done!")
+
+    cfg = config.load_config("mach2_validation_config.yaml")
+
+    ddf, delayed_dump, other_delayed = preprocess.preprocess(cfg)
+
+    compconf = cfg['compute']
+    compconf['verbose'] = cfg['verbose']
+
+    with compute.get_dask_compute_environment(compconf) as client:
+        f = client.persist(delayed_dump)
         progress(f)
         f.compute()
 
-    # TODO: add conditional probability computation with ..._per_config ?
- 
+    print("Validation Data Done!")
